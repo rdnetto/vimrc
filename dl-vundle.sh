d5cb59efe0df796c7dc2624eb6d5fad85ca08508
Reuben D'Netto <reuben.dnetto@calyptech.com>
2013-07-22 13:26:11 +1000

    Fixed dl-vundle.sh to install bundles properly

--- dl-vundle.sh
+++ dl-vundle.sh
@@ -2,5 +2,5 @@
 #This script downloads and installs Vundle
 mkdir ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
-echo "Please install exuberant-ctags"
+vim +BundleInstall +qa
+echo "Please install exuberant-ctags, ack-grep and compile YCM"
