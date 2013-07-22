1db449752c6bc40b66c2fea463e7ba5e01b38ccd
Reuben D'Netto <reuben.dnetto@calyptech.com>
2013-07-22 13:59:11 +1000

    Modified installation script to also perform bundle updates.

--- dl-vundle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#This script downloads and installs Vundle
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qa
-echo "Please install exuberant-ctags, ack-grep and compile YCM"
