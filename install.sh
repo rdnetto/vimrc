21abb0a4efc015353bfe234f64008b3a2a4d21ac
Reuben D'Netto <rdnetto@gmail.com>
2014-09-02 19:29:22 +1000

    Added YCM to installation script

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+set -e
+
+#Need to manually install the package manager, so it can pull everything else in
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# YCM is too big to be installed automatically (download times out)
+git clone --recursive https://github.com/Valloric/YouCompleteMe.git "~/.vim/bundle/YouCompleteMe" 
+~/.vim/bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
+
